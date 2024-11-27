f626a7d4e50d257996991aa66a996298c1016fcb
Benjaminvenegasmunoz <marcialvenegas608@gmail.com>
2024-11-27 14:13:09 -0400

    Metodos setColorCounter, iniciamos

--- app/src/main/java/com/example/threadscontrolador/MainActivity.java
+++ app/src/main/java/com/example/threadscontrolador/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.threadscontrolador;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,59 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button start;
+    private LinearLayout b1, b2, b3;
+    private int counter = 0;
+    private boolean iniciar = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        b1 = findViewById(R.id.bombillaVerde);
+        b2 = findViewById(R.id.bombillaAmarilla);
+        b3 = findViewById(R.id.bombillaRoja);
+        start = findViewById(R.id.btn_inicio);
+        b1.setBackgroundColor(getResources().getColor(R.color.gris));
+        b2.setBackgroundColor(getResources().getColor(R.color.gris));
+        b3.setBackgroundColor(getResources().getColor(R.color.gris));
+    }
+
+    private void setColorCounter(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (iniciar){
+                    counter++;
+                    switch (counter){
+                        case 1:
+                            b1.setBackgroundColor(getResources().getColor(R.color.verde));
+                            b2.setBackgroundColor(getResources().getColor(R.color.gris));
+                            b3.setBackgroundColor(getResources().getColor(R.color.gris));
+                            break;
+                        case 2:
+                            b1.setBackgroundColor(getResources().getColor(R.color.gris));
+                            b2.setBackgroundColor(getResources().getColor(R.color.amarillo));
+                            b3.setBackgroundColor(getResources().getColor(R.color.gris));
+                            break;
+                        case 3:
+                            b1.setBackgroundColor(getResources().getColor(R.color.gris));
+                            b2.setBackgroundColor(getResources().getColor(R.color.gris));
+                            b3.setBackgroundColor(getResources().getColor(R.color.rojo));
+                            break;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
